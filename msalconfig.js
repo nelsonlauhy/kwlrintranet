5764003a0e86b7e430202bdfa7a3daf7ab5bab34
nelsonlauhy <nelson.lau.hy@outlook.com>
2025-06-02 12:21:16 -0400

    update tarent id

--- msalconfig.js
+++ msalconfig.js
@@ -1,7 +1,7 @@
 const msalConfig = {
   auth: {
     clientId: 'cf01424e-21e3-4b98-bb96-93d91e32cf7b',
-    authority: 'https://login.microsoftonline.com/common', // Or your tenant ID
+    authority: '9f6c51b3-4040-49cf-a898-30bb9f2cfc92',
     redirectUri: window.location.origin
   },
   cache: {
